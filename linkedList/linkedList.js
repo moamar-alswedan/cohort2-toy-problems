bbb146f526772aae73be7e51a6bede7dc2fb19ca
moamar alswedan <bosraalsham@gmail.com>
2017-01-19 09:20:44 +0200

    linkedlist

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -11,14 +11,43 @@
 // list.addToTail(4);
 // list.addToTail(5);
 // list.head.value;   //yields '4';
-// list.contains(5);  //yields 'true';
-// list.contains(6);  //yields 'false';
+// list.contains(5);  //yields 'true'; // list.contains(6);  //yields 'false';
 // list.removeHead(); //yields '4'
 // list.tail.value;   //yields '5';
 
-
-var LinkedList = function(){
-  //fill me in!
+var LinkedList = function(){  
+  this.head = null;
+  this.tail = null;
 };
 
-//write methods here!
+LinkedList.prototype.Node = function(value){  
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
+
+LinkedList.prototype.addToTail = function(value){  
+  if (this.head === null) {
+    this.tail.next = null;
+    this.tail = this.Node(value);
+  } else {
+     this.head = this.Node(value);
+     this.tail = this.Node(value)
+  }
+};
+
+LinkedList.prototype.removeHead = function(){  
+  var temp = this.head.value;
+  if (this.head.value === this.tail.value) {
+      this.head = null;
+      this.tail = null ;
+  } else {
+      this.head = this.head.next;
+  }
+  return temp;
+};
+
+LinkedList.prototype.contains = function(value){  
+  
+};
