e5e700f119e0d36afc0a5a64fb80751d8bbd5238
moamar alswedan <bosraalsham@gmail.com>
2017-01-25 09:22:51 +0200

    spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -8,36 +8,29 @@
       [7,8,9]
     ]);
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
- */
+    */
 
 var spiralTraversal = function(matrix){
 	var arr = [];
-  var i = 0
-  var j = 0
+	var i = 0
+	var j = 0
 // we need to print first the border of the matrix 
 // then delete it and recursion 
-  var print = function(matrix){
-  for (i , j  ; j < matrix.length ; j++) {
-    arr.push(matrix[i][j])
-  }
-  for (var i , j=matrix.length-1; i < matrix.length; i++) {
-    arr.push(matrix[i][j])
-  }
-  for (var i = matrix.length-1, j = matrix.length-2 ; j >=0; j--) {
-    arr.push(matrix[i][j])
-  }
-  for (var i = matrix.length-2, j = 0 ; i>=1; i--) {
-    arr.push(matrix[i][j])
-  }
-}
-
-while( i < matrix.length, j < matrix.length){
-
-i++;
-j++
-}
-
-
+//  var print = function(matrix){
+	for (i , j  ; j < matrix.length ; j++) {
+		arr.push(matrix[i].pop())
+	}
+	for (var i , j=matrix[i].length-1; i < matrix.length; i++) {
+		arr.push(matrix[i].pop())
+	}
+	for (var i = matrix.length-1, j = matrix.length-2 ; j >=0; j--) {
+		arr.push(matrix[i].pop())
+	}
+	for (var i = matrix.length-2, j = 0 ; i>=1; i--) {
+		arr.push(matrix[i].pop())
+	}
+//}
 
 return arr
 };
+// Time complixity O(n*m)
