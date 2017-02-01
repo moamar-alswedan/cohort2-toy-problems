65535f1b8f098616ebc308aa962a268137f78c7d
moamar alswedan <bosraalsham@gmail.com>
2017-02-01 09:00:47 +0200

    facbook

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,20 +7,23 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
-var largestProductOfThree = function(array) {
+ var largestProductOfThree = function(array) {
+	// sort the array
 	array.sort(function(a, b){return a-b});
-	var small = array[0]*array[1]*array[array.length-1];
-	var large = array[array.length-1]*array[array.length-2]*array[array.length-3];
-	if(small< large) {
-		return large
-	} else {
+ 	// find the largestProductOfThree for negative number
+ 	var small = array[0]*array[1]*array[array.length-1];
+ 	// find the largestProductOfThree for positive number
+ 	var large = array[array.length-1]*array[array.length-2]*array[array.length-3];
+ 	if(small< large) {
+ 		return large
+ 	} else {
+ 		return small
+ 	}
 
-	return small
-	}
-	
-};
+ };
 
-console.log(largestProductOfThree([-5,2, 1, 3, 7,-2]))
+// time complixity = O(n)
+console.log(largestProductOfThree([-5, 2, 1, 3, 7,-2]))
 
 
 
