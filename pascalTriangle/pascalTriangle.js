2da29c3f9b46029a93b23e536f53791255ac661c
moamar alswedan <bosraalsham@gmail.com>
2017-02-05 09:20:15 +0200

    pascal

--- pascalTriangle/pascalTriangle.js
+++ pascalTriangle/pascalTriangle.js
@@ -18,13 +18,33 @@ http://en.wikipedia.org/wiki/Pascal's_triangle
 
 Also, see the animated GIF file in this folder (open it in Chrome)
 
-The task for this exercise is to write a function that given the number of rows, builds an array of arrays that represents the pascal triangle.
+The task for this exercise is to write a function that given the number of rows, 
+builds an array of arrays that represents the pascal triangle.
 
 For example:
 buildTriangle(3);// should output [[1],[1,1],[1,2,1]];
 */
+var sum =function(array){
+	// array to sum the number of array
+	var arr =[];
+	for (var i = 0; i < array.length-1; i++) {
+		arr.push(array[i]+array[i+1])
+	}
+	return arr
+}
 
 var buildTriangle = function(numOfRows){
-	
+	var arr = [];
+	arr[0]=[1]
+	arr[1]=[1,1]	
+	for (var i = 0; i < numOfRows; i++) {
+		arr.push(sum(i))
+		arr.push([1])
+
+	}
+	return arr
 }
-		
+// complixity time = O(n2)
+
+console.log(buildTriangle(3))		
+console.log(sum([1,1]))
